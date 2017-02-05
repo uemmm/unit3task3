f4b0b04e87ccfdba82422fac80b5aaafe99c16e9
uemmm <e.u1@arts.ac.uk>
2017-02-05 17:57:08 +0000

    Add files via upload

---
+++ unit3task3/sketch.js
@@ -0,0 +1,117 @@
+function setup() {
+  createCanvas(475, 525);
+  
+}
+function draw() {
+  strokeWeight(3);
+  fill('white');
+  rect(0,0,100,50);
+  fill('white');
+  rect(100,0,200,50);
+  fill('white');
+  rect(300,0,150,50);
+  fill('white');
+  rect(0,50,50,150);
+  fill(192,70,49);
+  rect(50,50,250,300); //red
+  fill('white');
+  rect(0,200,50,200);
+  fill(228,193,93);
+  rect(0,400,50,125);
+  fill(14,15,7);
+  rect(50,350,125,125);
+  fill('white');
+  rect(50,475,125,50);
+  rect(175,350,125,50);
+  rect(175,400,125,75);
+  fill(14,15,7);
+  rect(175,475,125,25); //black
+  rect(175,500,275,25);
+  fill(228,193,93);
+  rect(300,0,150,50); //yellow
+  fill(228,193,93);
+  rect(300,50,150,150); //yellow
+  fill('white');
+  rect(300,200,75,150);
+  rect(375,200,75,150);
+  rect(300,350,150,50);
+  fill(35,66,112);
+  rect(300,400,150,125); //blue
+  fill('white');
+  rect(450,0,25,400);
+  fill(192,70,49);
+  rect(450,400,25,125); //red
+  fill('white');
+  if (mouseIsPressed)
+    ellipse(50,50,50,50);
+  else
+    rect(0,0,100,50);
+  if(mouseIsPressed)
+    ellipse(0,50,50,50)
+  else
+    rect(0,50,50,150);
+  if(mouseIsPressed)
+    ellipse(175,350,50,50)
+  else
+    rect(175,350,125,50);
+  if(mouseIsPressed)
+    ellipse(300,200,50,50)
+  else
+    rect(300,200,75,150);
+  if(mouseIsPressed)
+    ellipse(175,500,50,50)
+  else
+    rect(175,500,275,25);
+  if(mouseIsPressed)
+    ellipse(50,475,50,50)
+  else
+    rect(50,475,125,50);
+  if(mouseIsPressed)
+    ellipse(0,400,50,50)
+  else
+    rect(0,400,50,125);
+  if(mouseIsPressed)
+    ellipse(450,400,50,50)
+  else
+    rect(450,400,25,125);
+  if(mouseIsPressed)
+    ellipse(375,200,50,50)
+  else
+    rect(375,200,75,150);
+  if(mouseIsPressed)
+    ellipse(50,350,50,50)
+  else
+    rect(50,350,125,125);
+  if(mouseIsPressed)
+    ellipse(100,0,50,50)
+  else
+    rect(100,0,200,50);
+  if(mouseIsPressed)
+    ellipse(300,350,50,50)
+  else
+    rect(300,350,150,50);
+  if(mouseIsPressed)
+    ellipse(300,350,50,50)
+  else
+    rect(300,350,150,50);
+  if(mouseIsPressed)
+    ellipse(300,0,50,50)
+  else
+    rect(300,0,150,50);  
+   if(mouseIsPressed)
+    ellipse(100,0,50,50)
+  else
+    rect(100,0,200,50);
+  if(mouseIsPressed)
+    ellipse(0,200,50,50)
+  else
+    rect(0,200,50,200);
+  if(mouseIsPressed)
+    ellipse(300,0,50,50)
+  else
+    rect(300,0,150,50); 
+  
+
+  
+}
+
